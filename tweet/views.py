0ff779dbeb59e4dd89b6cc03edbd1343830b32cc
Reginald Tjitradi <rtjitradi@gmail.com>
2020-09-11 12:17:12 -0700

    Converted functions to class based views for PostTweetView, UserProfileView, LogoutView, AddTweetView, FollowView, and UnfollowView

--- tweet/views.py
+++ tweet/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.views.generic import TemplateView
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from tweet.models import TweetModel
 from tweet.forms import AddTweetForm
@@ -7,15 +10,32 @@ from tweet.organizer import tweet_parser
 
 
 # Create your views here.
-@login_required
-def addtweet_view(request):
-    if request.method == 'POST':
+# @login_required
+# def addtweet_view(request):
+#     if request.method == 'POST':
+#         addtweet_form = AddTweetForm(request.POST)
+#         if addtweet_form.is_valid():
+#             addtweet_data = addtweet_form.cleaned_data
+#             tweet = TweetModel.objects.create(characters=addtweet_data.get('characters'), user=request.user)
+#             tweet_parser(tweet)
+#             return HttpResponseRedirect(reverse('homepage'))
+
+#     addtweet_form = AddTweetForm()
+#     return render(request, 'generic_form.html', {'page_title': 'TwitterClone', 'form': addtweet_form})
+
+
+class AddTweetView(LoginRequiredMixin, TemplateView):
+    
+    def get(self, request):
+        addtweet_form = AddTweetForm()
+        return render(request, 'generic_form.html', {'page_title': 'TwitterClone', 'form': addtweet_form})
+    
+    def post(self, request):
         addtweet_form = AddTweetForm(request.POST)
         if addtweet_form.is_valid():
             addtweet_data = addtweet_form.cleaned_data
             tweet = TweetModel.objects.create(characters=addtweet_data.get('characters'), user=request.user)
             tweet_parser(tweet)
             return HttpResponseRedirect(reverse('homepage'))
-
-    addtweet_form = AddTweetForm()
-    return render(request, 'generic_form.html', {'page_title': 'TwitterClone', 'form': addtweet_form})
+        else:
+            return render(request, 'generic_form.html', {'page_title': 'TwitterClone', 'form': addtweet_form})
